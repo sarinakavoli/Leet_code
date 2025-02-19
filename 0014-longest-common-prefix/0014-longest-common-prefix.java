1551ba83f1c61afbd30fdfbbfff228ebe8b2afb7
sarinakavoli <157720748+sarinakavoli@users.noreply.github.com>
2025-02-19 11:33:46 -0500

    Time: 0 ms (100%), Space: 42 MB (26.02%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+
+        String perfix = strs[0];
+        for(int i=0; i<strs.length; i++){
+            while(strs[i].indexOf(perfix) != 0){
+               perfix = perfix.substring(0,perfix.length()-1);
+               if(perfix.isEmpty()){
+                    return "";
+                }
+
+            }
+        }
+        return perfix;
+        
+    }
+}
